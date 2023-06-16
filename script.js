a7d471b3dda35521b8b3bb79da9c53e5eee1bc84
Michael <pxmyhdev@gmail.com>
2023-06-15 23:11:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Define the options on the spin wheel
-var options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"];
-
 function spinWheel() {
-    // Spin the wheel and select a random option
-    var selectedOption = options[Math.floor(Math.random() * options.length)];
-    document.getElementById("result").textContent = "You spun the wheel and got: " + selectedOption;
+    fetch('options.json')
+        .then(response => response.json())
+        .then(data => {
+            var options = data.options;
+            var selectedOption = options[Math.floor(Math.random() * options.length)];
+            document.getElementById("result").textContent = "You spun the wheel and got: " + selectedOption;
+        })
+        .catch(error => console.error('Error:', error));
 }
