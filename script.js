42610c673589b311135ad002240acefbcb47cb9a
Michael <pxmyhdev@gmail.com>
2023-06-17 14:51:36 -0400

    Show the texts when page is loaded

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function spinWheel() {
+document.addEventListener("DOMContentLoaded", function() {
     fetch('options.json')
         .then(response => response.json())
         .then(data => {
             var options = data.options;
-            var selectedOption = options[Math.floor(Math.random() * options.length)];
-            document.getElementById("result").textContent = "You spun the wheel and got: " + selectedOption;
 
             var canvas = document.getElementById("canvas");
             var ctx = canvas.getContext("2d");
@@ -36,11 +34,28 @@ function spinWheel() {
                 ctx.fillText(option, 0, 0);
                 ctx.restore();
             });
+        })
+        .catch(error => console.error('Error:', error));
+});
+
+function spinWheel() {
+    fetch('options.json')
+        .then(response => response.json())
+        .then(data => {
+            var options = data.options;
+            var selectedOption = options[Math.floor(Math.random() * options.length)];
+            document.getElementById("result").textContent = "You spun the wheel and got: " + selectedOption;
+
+            var canvas = document.getElementById("canvas");
+            var ctx = canvas.getContext("2d");
 
             // Wheel animation
             var duration = 5000; // Duration of the spin animation in milliseconds
             var startAngle = Math.random() * Math.PI * 2; // Starting angle of rotation
             var endAngle = startAngle + Math.PI * 2; // Ending angle of rotation
+
+            var centerX = canvas.width / 2;
+            var centerY = canvas.height / 2;
 
             var startTime = null;
 
@@ -51,13 +66,13 @@ function spinWheel() {
 
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 options.forEach((option, index) => {
-                    var optionAngle = index * (Math.PI * 2 / options.length) + angle + rotationOffset;
-                    var x = centerX + (wheelRadius - 20) * Math.cos(optionAngle);
-                    var y = centerY + (wheelRadius - 20) * Math.sin(optionAngle);
+                    var optionAngle = index * (Math.PI * 2 / options.length) + angle;
+                    var x = centerX + (canvas.width / 2 - 20) * Math.cos(optionAngle);
+                    var y = centerY + (canvas.height / 2 - 20) * Math.sin(optionAngle);
 
                     ctx.save();
                     ctx.translate(x, y);
-                    ctx.rotate(optionAngle + Math.PI);
+                    ctx.rotate(optionAngle + Math.PI / 2);
                     ctx.fillText(option, 0, 0);
                     ctx.restore();
                 });
