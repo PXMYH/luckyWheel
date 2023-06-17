6f451c4a8d4c2a6ea87caff2595faa2b130c33e6
Michael <pxmyhdev@gmail.com>
2023-06-17 14:27:23 -0400

    Rotate the wheel

--- script.js
+++ script.js
@@ -31,6 +31,46 @@ function spinWheel() {
                 ctx.fillText(option, 0, 0);
                 ctx.restore();
             });
+
+            // Wheel animation
+            var duration = 5000; // Duration of the spin animation in milliseconds
+            var startAngle = Math.random() * Math.PI * 2; // Starting angle of rotation
+            var endAngle = startAngle + Math.PI * 2; // Ending angle of rotation
+
+            var startTime = null;
+
+            function animateWheel(timestamp) {
+                if (!startTime) startTime = timestamp;
+                var elapsedTime = timestamp - startTime;
+                var angle = easeInOutCubic(elapsedTime, startAngle, endAngle - startAngle, duration);
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                options.forEach((option, index) => {
+                    var optionAngle = index * (Math.PI * 2 / options.length) + angle;
+                    var x = centerX + wheelRadius * Math.cos(optionAngle);
+                    var y = centerY + wheelRadius * Math.sin(optionAngle);
+
+                    ctx.save();
+                    ctx.translate(x, y);
+                    ctx.rotate(optionAngle + Math.PI / 2);
+                    ctx.fillText(option, 0, 0);
+                    ctx.restore();
+                });
+
+                if (elapsedTime < duration) {
+                    requestAnimationFrame(animateWheel);
+                }
+            }
+
+            requestAnimationFrame(animateWheel);
         })
         .catch(error => console.error('Error:', error));
+}
+
+// Easing function for smooth animation
+function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
 }
